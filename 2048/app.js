75bd6a8525c99061d12cdde04662b3ac84c60c36
Patrik Přidal <98656826+vibemane@users.noreply.github.com>
2022-03-04 19:59:11 +0100

    move down

--- 2048/app.js
+++ 2048/app.js
@@ -83,6 +83,34 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    // swipe down
+    function moveDown() {
+        for (let i=0; i < 4; i++) {
+            let totalOne = squares[i].innerHTML
+            let totalTwo = squares[i+width].innerHTML
+            let totalThree = squares[i+(width*2)].innerHTML
+            let totalFour = squares[i+(width*3)].innerHTML
+            let column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+            let fiteredColumn = column.filter(num => num)
+            let missing = 4 - filteredColumn.length
+            let zeros = Array(missing).fill(0)
+            let newColumn = zeros.concat(filteredColumn)
+
+            squares[i].innerHTML = newColumn[0]
+            squares[i+width].innerHTML = newColumn[1]
+            squares[i+(width*2)].innerHTML = newColumn[2]
+            squares[i+(width*3)].innerHTML = newColumn[3]
+        }
+    }
+
+
+
+
+
+
+
+
     function combineRow() {
         for (let i=0; i < 15; i++) {
             if (squares[i].innerHTML === squares[i+1].innerHTML) {
