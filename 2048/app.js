86600c733e71da6e2e99b50f99859e2472f4afc1
Patrik Přidal <98656826+vibemane@users.noreply.github.com>
2022-03-04 19:26:15 +0100

    moveRight

--- 2048/app.js
+++ 2048/app.js
@@ -27,7 +27,37 @@ document.addEventListener('DOMContentLoaded', () => {
         } else generate()
     }
 
+    // swipe right
+    function moveRight() {
+        for (let i=0; i < 16; i++) {
+            if(i % 4 === 0) {
+                let totalOne = squares[i].innerHTML
+                let totalTwo = squares[i+1].innerHTML
+                let totalThree = squares[i+2].innerHTML
+                let totalFour = squares[i+3].innerHTML
+                let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+                
 
+                console.log(row)
+
+                let filteredRow = row.filter(num => num)
+                console.log(filteredRow)
+                let missing = 4 - filteredRow.length
+                let zeros = Array(missing).fill(0)
+                console.log(zeros)
+                let newRow = zeros.concat(filteredRow)
+                console.log(newRow)
+
+                squares[i].innerHTML = newRow[0]
+                squares[i+1].innerHTML = newRow[1]
+                squares[i+2].innerHTML = newRow[2]
+                squares[i+3].innerHTML = newRow[3]
+                
+            }
+        }
+    }
+
+    moveRight()
 
 
 
