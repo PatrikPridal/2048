2e0e55d821014d3cf534a426db47c59f26f3c341
Patrik Přidal <98656826+vibemane@users.noreply.github.com>
2022-03-05 11:57:00 +0100

    2048 done (logic only)

--- 2048/app.js
+++ 2048/app.js
@@ -198,7 +198,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // check for the nuber 2048 in the squares to win
     function checkForWin() {
         for (let i=0; i < squares.length; i++) {
-            if (squares[i].innerHTML == 2048) {
+            if (squares[i].innerHTML == 12048) {
                 resultDisplay.innerHTML = 'You Win!'
                 document.removeEventListener('keyup', control)
             }
